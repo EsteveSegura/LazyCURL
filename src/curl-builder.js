6aa7c33bd4c64e9e52d738c2f49f48c2d47020d4
Esteve Segura <esteve.segura@voicemod.net>
2024-02-28 22:49:16 +0100

    feat; add curl abstractions

---
+++ src/curl-builder.js
@@ -0,0 +1,19 @@
+class CurlBuilder {
+    constructor({url}) {
+        this.url = url;
+    }
+
+    build() {
+        return `curl ${this.url}`;
+    }
+}
+
+export default CurlBuilder;
+
+/*    
+let curlCommand = `curl -X ${method} ${headers} ${url}`;
+    if (data) {
+        curlCommand += ` --data '${data}'`;
+    }
+*/
+
---
+++ src/curl-launcher.js
@@ -0,0 +1,23 @@
+import { spawn } from 'child_process';
+
+class CurlLauncher {
+    constructor({ curlCommand }) {
+        this._curlProcess = spawn(curlCommand, { shell: true });
+    }
+
+    launch() {
+        this._curlProcess.stdout.on('data', data => {
+            console.log(`stdout: ${data}`);
+        });
+
+        this._curlProcess.stderr.on('data', data => {
+            console.error(`stderr: ${data}`);
+        });
+
+        this._curlProcess.on('close', code => {
+            console.log(`child process exited with code ${code}`);
+        });
+    }
+}
+
+export default CurlLauncher;
