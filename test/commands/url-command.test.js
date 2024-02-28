066642bbfbe0d2ae17a9b5ff64a8db79fb2d584f
Esteve Segura <esteve.segura@voicemod.net>
2024-02-29 00:15:09 +0100

    test: add UrlCommand test and remove dummy test

---
+++ test/commands/url-command.test.js
@@ -0,0 +1,70 @@
+import Url from '../../src/commands/url-command/';
+import InvalidUrlFormat from '../../src/commands/url-command/invalid-url-format.js';
+
+describe('UrlCommand', () => { 
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should have name and message properties', () => {
+        const urlCommand = new Url();
+        
+        expect(urlCommand.name).toBe("url");
+        expect(urlCommand.message).toBe("Introduce la URL:");
+    });
+
+    describe('should run "validate" without any errors if the url is ok', () => {
+        test('url: https://www.google.com', () => {
+            const urlCommand = new Url();
+            const url = "https://www.google.com";
+            
+            const result = urlCommand.validate({ url });
+            
+            expect(result).toBe(true);
+        });
+    
+        test('url: https://www.google', () => {
+            const urlCommand = new Url();
+            const url = "https://www.google";
+            
+            const result = urlCommand.validate({ url });
+            
+            expect(result).toBe(true);
+        });
+
+        test('url: https://google', () => {
+            const urlCommand = new Url();
+            const url = "https://google";
+            
+            const result = urlCommand.validate({ url });
+            
+            expect(result).toBe(true);
+        });
+
+        test('url: http://google', () => {
+            const urlCommand = new Url();
+            const url = "http://google";
+            
+            const result = urlCommand.validate({ url });
+            
+            expect(result).toBe(true);
+        });
+    });
+
+    
+    describe('should throw error when running "validate" with incorrect urls', () => {
+        it('url: www.google.com', () => {
+            const urlCommand = new Url();
+            const url = "www.google.com";
+            
+            expect(() => urlCommand.validate({ url })).toThrow(InvalidUrlFormat);
+        });
+
+        it('url: anytext', () => {
+            const urlCommand = new Url();
+            const url = "anytext";
+            
+            expect(() => urlCommand.validate({ url })).toThrow(InvalidUrlFormat);
+        });
+    })
+});
