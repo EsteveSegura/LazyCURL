c3b95c7a2a7030f196928ec2c77b5f92966a694d
Esteve Segura <esteve.segura@voicemod.net>
2024-03-02 00:40:39 +0100

    test: add confirmation.test.js for CLI testing

---
+++ test/cli/confirmation.test.js
@@ -0,0 +1,24 @@
+import confirmation from "../../src/cli/confirmation.js";
+
+describe("confirmation", () => {
+    test("should return a object with the properties", () => {
+        const result = confirmation({name: "headers", message: "¿Desea ejecutar el comando curl?", valueDefault: false});
+        expect(result).toEqual({
+            type: 'confirm',
+            name: 'askheaders',
+            message: '¿Desea ejecutar el comando curl?',
+            default: false,
+            prefix: 'ask',
+            onlyName: 'headers'
+        });
+    });
+
+    test("should throw an error if name is not provided", () => {
+        expect(() => confirmation({message: "¿Desea ejecutar el comando curl?", valueDefault: false})).toThrow("name and message are required");
+    });
+
+    test("should throw an error if message is not provided", () => {
+        expect(() => confirmation({name: "headers", valueDefault: false})).toThrow("name and message are required");
+    });
+    
+});
