db650bbebd5a081f10b415426fcc64ad57be28ca
Esteve Segura <esteve.segura@voicemod.net>
2024-03-02 03:17:06 +0100

    feat: add debug option to CurlLauncher class

--- src/infrastructure/services/curl/curl-launcher.js
+++ src/infrastructure/services/curl/curl-launcher.js
@@ -3,6 +3,7 @@ import { spawn } from "child_process";
 class CurlLauncher {
     constructor({ curlCommand }) {
         this._curlProcess = spawn(curlCommand, { shell: true });
+        this.debug = false;
     }
 
     launch() {
@@ -10,13 +11,15 @@ class CurlLauncher {
             console.log(`stdout: ${data}`);
         });
 
-        this._curlProcess.stderr.on("data", data => {
-            console.error(`stderr: ${data}`);
-        });
+        if (this.debug) {
+            this._curlProcess.stderr.on("data", data => {
+                console.error(`stderr: ${data}`);
+            });
 
-        this._curlProcess.on("close", code => {
-            console.log(`child process exited with code ${code}`);
-        });
+            this._curlProcess.on("close", code => {
+                console.log(`child process exited with code ${code}`);
+            });
+        }
     }
 }
 
