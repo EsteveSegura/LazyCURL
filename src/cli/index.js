f04af5d58b4c8b1e5474a73dd934971db22df8dd
Esteve Segura <esteve.segura@voicemod.net>
2024-03-02 03:21:32 +0100

    feat: add headers command to CLI menu

--- src/cli/index.js
+++ src/cli/index.js
@@ -6,11 +6,18 @@ const urlCommand = new UrlCommand();
 import MethodCommand from "../commands/method-command/index.js";
 const methodCommand = new MethodCommand();
 
+import HeadersCommand from "../commands/headers-command/index.js";
+const headersCommand = new HeadersCommand();
+
 import CurlBuilder from "../infrastructure/services/curl/curl-builder.js";
 import CurlLauncher from "../infrastructure/services/curl/curl-launcher.js";
 
+
+import confirmation from "./confirmation.js";
+const confirmationHeaders = confirmation({name: headersCommand.name, message: "¿Desea ejecutar el comando curl?", valueDefault: false});
+
 async function menuBuild() {
-    const { url, method } = await inquirer.prompt([
+    const { url, method, headers } = await inquirer.prompt([
         {
             type: "input",
             name: urlCommand.name,
@@ -22,9 +29,18 @@ async function menuBuild() {
             name: methodCommand.name,
             message: methodCommand.message,
             choices: methodCommand.choices
+        },
+        confirmationHeaders,
+        {
+            type: "input",
+            name: headersCommand.name,
+            message: headersCommand.message,
+            validate: (value) => {return headersCommand.validate({headerString: value}) },
+            filter: (value) => {return headersCommand.filter({headerString: value}) },
+            when: answers => answers[`${confirmationHeaders.prefix}${headersCommand.name}`]
         }
     ]);
-    const curlBuilder = new CurlBuilder({url ,method});
+    const curlBuilder = new CurlBuilder({url, method, headers});
     const curlCommand = curlBuilder.build();
 
     const curlLauncher = new CurlLauncher({curlCommand});
