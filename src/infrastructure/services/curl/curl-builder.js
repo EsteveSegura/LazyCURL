232c4972f34ebcd6d5054869e1641d65165bceb7
Esteve Segura <esteve.segura@voicemod.net>
2024-03-05 23:59:45 +0100

    feat: refactor CurlBuilder class to include data and askContentType properties

--- src/infrastructure/services/curl/curl-builder.js
+++ src/infrastructure/services/curl/curl-builder.js
@@ -1,9 +1,11 @@
 class CurlBuilder {
-    constructor({url = "", method = "GET", headers = "", includeHeaders = false}) {
+    constructor({url = "", method = "GET", headers = "", includeHeaders = false,  data = null, askContentType = null}) {
         this.url = url;
         this.method = method;
         this.headers = headers.trim();
         this.includeHeaders = includeHeaders;
+        this.data = data;
+        this.askContentType = askContentType;
     }
 
     build() {
@@ -12,7 +14,9 @@ class CurlBuilder {
             this.method,
             this._includeHeadersTranslate(this.includeHeaders),
             this.url,
-            this.headers
+            this.headers,
+            this._askContentTypeTranslate(this.askContentType),
+            this._dataTranslate(this.data)
         ];
 
         return components.filter(component => component).join(" ");
@@ -21,13 +25,14 @@ class CurlBuilder {
     _includeHeadersTranslate(value) {
         return value ? "-i" : "";
     }
+
+    _askContentTypeTranslate(value) {
+        return value ? `-H "Content-Type: ${value}"` : "";
+    }
+
+    _dataTranslate(value) {
+        return value ? `--data '${value}'` : "";
+    }
 }
 
 export default CurlBuilder;
-
-/*    
-let curlCommand = `curl -X ${method} ${headers} ${url}`;
-    if (data) {
-        curlCommand += ` --data '${data}'`;
-    }
-*/
