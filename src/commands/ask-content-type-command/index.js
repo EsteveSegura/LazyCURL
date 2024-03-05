18394b7361ddbf42ca0fc36844655a75b858a954
Esteve Segura <esteve.segura@voicemod.net>
2024-03-05 23:58:30 +0100

    feat: add AskContentType command class

---
+++ src/commands/ask-content-type-command/index.js
@@ -0,0 +1,9 @@
+class AskContentType {
+    constructor() {
+        this.name = "askContentType";
+        this.message = "Que formato tiene el payload? :";
+        this.choices =  ["application/json", "application/xml", "application/x-www-form-urlencoded", "multipart/form-data"];
+    }
+}
+
+export default AskContentType;
