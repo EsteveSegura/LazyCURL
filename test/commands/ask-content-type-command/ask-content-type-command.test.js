a9858c7eee155fbcc492402e971077baba0360d6
Esteve Segura <esteve.segura@voicemod.net>
2024-03-05 23:58:44 +0100

    test: add test for AskContentTypeCommand

---
+++ test/commands/ask-content-type-command/ask-content-type-command.test.js
@@ -0,0 +1,11 @@
+import AskContentType from "../../../src/commands/ask-content-type-command";
+
+describe('AskContentTypeCommand', () => {
+    it('should have name and message properties', () => {
+        const askContentTypeCommand = new AskContentType();
+        
+        expect(askContentTypeCommand.name).toBe("askContentType");
+        expect(askContentTypeCommand.message).toBe("Que formato tiene el payload? :");
+        expect(askContentTypeCommand.choices).toEqual(["application/json", "application/xml", "application/x-www-form-urlencoded", "multipart/form-data"]);
+    });
+});
