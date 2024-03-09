50bdadd0b63be3e2d0e210ae69d4dcbf0faa714a
Esteve Segura <esteve.segura@voicemod.net>
2024-03-09 21:50:33 +0100

    feat: add verbose command class

---
+++ src/commands/verbose-command/index.js
@@ -0,0 +1,8 @@
+class Verbose {
+    constructor() {
+        this.name = "verbose";
+        this.message = "Quieres obtener información detallada de la petición (debug) ?:";
+    }
+}
+
+module.exports = Verbose;
