b80944fffbd0af38db764c0ae45ee4a9c39982b9
Esteve Segura <esteve.segura@voicemod.net>
2024-03-09 21:10:43 +0100

    feat: add insecure command class

---
+++ src/commands/insecure-command/index.js
@@ -0,0 +1,8 @@
+class Insecure {
+    constructor() {
+        this.name = "insecure";
+        this.message = "Quieres permitir peticiones inseguras (No verificar HTTPS) ?:";
+    }
+}
+
+module.exports = Insecure;
