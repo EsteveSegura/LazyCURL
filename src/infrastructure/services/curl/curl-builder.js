02179d8ad1da5df46ecb4a24e41f7fecd8ed29ff
Esteve Segura <esteve.segura@voicemod.net>
2024-03-09 16:14:31 +0100

    feat: add output option to CurlBuilder class

--- src/infrastructure/services/curl/curl-builder.js
+++ src/infrastructure/services/curl/curl-builder.js
@@ -1,11 +1,12 @@
 class CurlBuilder {
-    constructor({url = "", method = "GET", headers = "", includeHeaders = false,  data = null, askContentType = null}) {
+    constructor({url = "", method = "GET", headers = "", includeHeaders = false,  data = null, askContentType = null, output = null}) {
         this.url = url;
         this.method = method;
         this.headers = headers.trim();
         this.includeHeaders = includeHeaders;
         this.data = data;
         this.askContentType = askContentType;
+        this.output = output;
     }
 
     build() {
@@ -13,11 +14,21 @@ class CurlBuilder {
             "curl -X",
             this.method,
             this._includeHeadersTranslate(this.includeHeaders),
-            this.url,
             this.headers,
             this._askContentTypeTranslate(this.askContentType),
-            this._dataTranslate(this.data)
+            this._dataTranslate(this.data),
+            this._outputTranslate(this.output),
+            this.url,
         ];
+
+        // If the user wants to save the response in a file,
+        // then the -i flag is going to created interferes 
+        // with the output file, so it is removed.
+        // Remove from componentes the flag -i 
+        if(this.output) {
+            components.splice(2, 1);
+            // TODO: Add a warning message to the user, to let them know that the -i flag was removed
+        }
 
         return components.filter(component => component).join(" ");
     }
@@ -32,6 +43,10 @@ class CurlBuilder {
 
     _dataTranslate(value) {
         return value ? `--data '${value}'` : "";
+    }
+
+    _outputTranslate(value) {
+        return value ? `--output ${value}` : "";
     }
 }
 
