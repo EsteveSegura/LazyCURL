66961cccdc0430cd33a42c295d462de711e84deb
Esteve Segura <esteve.segura@voicemod.net>
2024-03-10 01:25:36 +0100

    test: update UserAgentCommand test with suggestions

--- test/commands/user-agent-command/user-agent-command.test.js
+++ test/commands/user-agent-command/user-agent-command.test.js
@@ -5,6 +5,10 @@ describe('UserAgentCommand', () => {
         const userAgentCommand = new UserAnget();
         
         expect(userAgentCommand.name).toBe("userAgent");
-        expect(userAgentCommand.message).toBe("Enter the User-Agent:");
+        expect(userAgentCommand.message).toBe("Enter the User-Agent (TAB to select):");
+        expect(userAgentCommand.suggestions).toBeInstanceOf(Array);
+        expect(userAgentCommand.suggestions.length).toBe(999);
+        expect(userAgentCommand.suggestions[0]).toBe("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36");
+        expect(userAgentCommand.suggestions[998]).toBe("Mozilla/5.0 (iPhone; CPU iPhone OS 8_4 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) GSA/7.0.55539 Mobile/12H143 Safari/600.1.4");
     });
 });
