c97db1e66d8b4b960d9e1b8c6384a708af3a13e0
Esteve Segura <esteve.segura@voicemod.net>
2024-03-22 20:29:23 +0100

    feat: add PrintTheCommandCommand class with choices for launching or printing command

---
+++ src/commands/print-the-command-command/index.js
@@ -0,0 +1,9 @@
+class PrintTheCommandCommand {
+    constructor() {
+        this.name = "printTheCommand";
+        this.message = "Do you want to launch the command or print it on screen?"
+        this.choices =  ["Launch command", "Print command on screen"];
+    }
+}
+
+module.exports = PrintTheCommandCommand;
